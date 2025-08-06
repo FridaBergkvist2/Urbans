6fdbd3ab042424a56ff2c3d64d45639098c330c8
FridaBergkvist2 <fridaber10@student.astar.se>
2025-08-06 14:28:39 +0200

    Uppdaterar de gamla filerna.

---
+++ Urbans/Grönsaker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Produkt
+{
+	private string namn;
+	private double antal;	
+
+	public string Namn
+	{ 
+		get {return namn; } 
+		set 
+		{
+			if (!string.IsNullOrWhiteSpace(value))
+			    namn = value;
+		}
+	}
+
+	public double Antal
+	{
+		get {return antal;}
+		set
+		{
+			if (value>=0)
+			antal = value;
+		}
+	}
+	
+	public Produkt (string namn, double antal)
+	{
+		Namn = namn;
+		Antal = antal;
+	}
+	
+	public void VisaInfo()
+	{
+		Console.WriteLine($"Namn: {Namn}, Antal: {Antal}");
+	}
+}
+
---
+++ Urbans/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		List<Produkt> produkter = new List<Produkt>();
+		bool kör = true;
+
+		while (kör)
+		{
+			Console.Write("Ange grönsak (eller skriv 'stopp' för att avsluta):");
+			string namn = Console.ReadLine();
+
+			if (namn.ToLower() == "stopp")
+			{
+				kör = false;
+				continue;
+			}	
+
+			Console.Write("Ange antal:");
+			double antal = Convert.ToDouble(Console.ReadLine());
+
+			produkter.Add(new Produkt(namn,antal));
+		}
+
+		Console.WriteLine("\n--- Grönsakslista ---");
+		foreach (Produkt p in produkter)
+		{
+			p.VisaInfo();
+		}
+	}
+}
+
+
+
+
